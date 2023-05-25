1e69c325c735fc38a7d0c7e2c7d1f2ef83118930
Jonathan Cifuentes <jcifuentes773@gmail.com>
2023-05-25 11:39:06 -0700

    upload to class 5

---
+++ CopyOfClass6/class6.R
@@ -0,0 +1,4 @@
+# Example input vectors to start with
+student1 <- c(100, 100, 100, 100, 100, 100, 100, 90)
+student2 <- c(100, NA, 90, 90, 90, 90, 97, 80)
+student3 <- c(90, NA, NA, NA, NA, NA, NA, NA)
